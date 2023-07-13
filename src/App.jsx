fe9762c8e671828f64f6692091aa6606e87f9d56
Eldon-498 <eldondaniel2@gmail.com>
2023-07-13 22:53:32 +0300

    get subreddits

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,17 @@ function App (){
   const [data, setData] = useState([]);
 
   useEffect(()=>{
-    fetch('https://www.reddit.com/r/programming/top.json').
+    fetch('https://www.reddit.com/subreddits.json').
     then((response)=> {
       if(!response.ok){
         throw new Error('Network response was not ok');
       }
+      
       return response.json();
       //response.json;
     })
     .then((data) => {
+      console.log(data.data.children);
       const topPosts = data.data.children.map((posts)=> posts.data);
       setData(topPosts);
 
