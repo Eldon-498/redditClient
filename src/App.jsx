9cb578f8b551bd9ac255ed332076525e429f9df1
Eldon-498 <eldondaniel2@gmail.com>
2023-07-13 21:55:49 +0300

    top posts list fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,46 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
 import './App.css'
 
 function App (){
-  return <h1>HelloReddit</h1>
+
+  const [data, setData] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://www.reddit.com/r/programming/top.json').
+    then((response)=> {
+      if(!response.ok){
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+      //response.json;
+    })
+    .then((data) => {
+      const topPosts = data.data.children.map((posts)=> posts.data);
+      setData(topPosts);
+
+     
+      }).
+    catch((error) =>{
+      console.log("Error fecthing data");
+      throw error;
+    });
+    
+  },[]);
+
+
+   return ( 
+    <>
+    <h1>HelloReddit</h1>
+    <ul>
+       {data.map((data) => {
+         return <li key={data.id}>{data.title}</li>;
+       })}
+     </ul>
+     </>
+     );
+
+
+  
 }
 export default App;
