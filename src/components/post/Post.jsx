87c54ec8ba92f548f4a955620974082564117a96
Eldon-498 <eldondaniel2@gmail.com>
2023-08-18 19:28:06 +0300

    Styling

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,16 +1,12 @@
 /* eslint-disable react/prop-types */
 import './post.css';
 
-
 const Post = ({ post }) => {
     const {title, thumbnail, likes, num_comments, author_fullname, timePosted} = post;
     console.log("post data", post);
     if (!post) {
         return <div>Loading...</div>;
-    }
-    
-
-
+    } 
     return ( 
         <div className='post'>
             <h2>{title}</h2>
